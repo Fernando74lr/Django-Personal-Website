6c4b1ed89d41824092654d2b3ea3d6f2e0dad062
Fernando <flopezramirez@hotmail.com>
2020-06-29 18:22:20 -0500

    Serve Media Files in Django Admin Panel

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     # Every field represents a field in the table.
     title = models.CharField(max_length=100, verbose_name="Title")
     description = models.TextField(verbose_name="Description")
-    image = models.ImageField(verbose_name="Image")
+    image = models.ImageField(verbose_name="Image", upload_to="projects")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Date of creation")
     updated = models.DateTimeField(auto_now=True, verbose_name="Date of modification")
 
--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about-me/', views.about, name='about'),
@@ -24,3 +26,11 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+# Extended configuration so we can serve media files.
+if settings.DEBUG:
+    # 'static' allow us to serve static files.
+    from django.conf.urls.static import static
+    # Add the path to the media file
+    # static(path_where_we_want_to_ser_the_media_files, the_root_where_it_has_to_look_for_the_media_files)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
