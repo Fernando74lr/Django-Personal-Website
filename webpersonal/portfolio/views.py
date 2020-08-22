f429e7d6c3b7487a19ea43f9b18f672ea23d4aa4
Fernando López Ramírez <39963559+Fernando74lr@users.noreply.github.com>
2020-08-21 21:49:16 -0500

    Update views.py

--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Project
 
-# Create your views here.
 def portfolio(request):
     # Return all the projects that has our model Project.
     projects = Project.objects.all()
