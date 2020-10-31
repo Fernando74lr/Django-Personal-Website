e7b2dcb3a78cc5d8b44b0b63ee2973088f430c02
unknown <flopezramirez@hotmail.com>
2020-10-30 20:40:36 -0600

    Starting Over

--- webpersonal/portfolio/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Project
-
-# Render projects
-def portfolio(request):
-    # Return all the projects that has our model Project.
-    projects = Project.objects.all()
-    return render(request, "portfolio/portfolio.html", {'projects':projects})
